e9f13bc975f29be9503a1e2a062d71788b93b281
Neelabh-Sharma <neelabhsharma2002@gmail.com>
2025-01-15 21:50:31 -0800

    latest update

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 
 exports.connectToDB=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect("MONGO_URI=mongodb+srv://neelabhsharma2002:G8VQpksBpdPHl5Yv@cluster0.jgfff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log('connected to DB');
     } catch (error) {
         console.log(error);
